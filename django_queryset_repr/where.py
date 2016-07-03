86ebecafe29120c9626b99f3781e520c1bb0809e
Alexey Kotlyarov <a@koterpillar.com>
2016-07-04 09:30:54 +1000

    repr for basic filtering

--- django_queryset_repr/where.py
+++ django_queryset_repr/where.py
@@ -1,6 +1,11 @@
 """Representation of where clauses."""
 
-from django.db.models.sql.where import NothingNode
+from django.db.models.sql.where import (
+    AND,
+    NothingNode,
+    WhereNode,
+)
+from django.db.models.expressions import Col
 
 
 def where_repr(where):
@@ -8,6 +13,27 @@ def where_repr(where):
 
     if isinstance(where, NothingNode):
         return 'none()'
+    if not isinstance(where, WhereNode):
+        raise NotImplementedError
     if not where.children:
         return 'all()'
-    raise NotImplementedError
+    if where.connector != AND:
+        raise NotImplementedError
+    return 'filter({})'.format(', '.join(
+        lookup_repr(lookup)
+        for lookup in where.children
+    ))
+
+
+def lookup_repr(lookup):
+    """Representation of a lookup."""
+
+    lookup_name = '' if lookup.lookup_name == 'exact' else \
+        '__' + lookup.lookup_name
+    if not isinstance(lookup.lhs, Col):
+        raise NotImplementedError
+    lhs = lookup.lhs.target.name
+    # FIXME: This should use qs_repr
+    rhs = repr(lookup.rhs)
+
+    return '{}{}={}'.format(lhs, lookup_name, rhs)
