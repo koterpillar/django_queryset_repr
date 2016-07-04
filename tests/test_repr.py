00de7fab398dc1b80e13178ce54e1a453e029534
Alexey Kotlyarov <a@koterpillar.com>
2016-07-04 15:38:15 +1000

    Test that the querysets are reproducible via their representations

--- tests/test_repr.py
+++ tests/test_repr.py
@@ -1,6 +1,7 @@
 """Test qs_repr()."""
 
 from django_queryset_repr import qs_repr
+from django_queryset_repr.utils.deep_eq import assert_deep_eq
 
 from .base import TestCase
 
@@ -8,11 +9,19 @@ from .base import TestCase
 class ReprTestCase(TestCase):
     """Test qs_repr()."""
 
+    def assert_reproducible(self, queryset):
+        """Assert a queryset is reproducible using its representation."""
+
+        repr_ = qs_repr(queryset)
+        new_queryset = eval(repr_, self.models.__dict__)
+        assert_deep_eq(queryset, new_queryset)
+
     def test_all(self):
         """Test representation of .all()."""
 
         qs_all = self.models.Tree.objects.all()
         self.assertEqual(qs_repr(qs_all), 'Tree.objects.all()')
+        self.assert_reproducible(qs_all)
 
     def test_filter(self):
         """Test representation of a filtered queryset."""
@@ -20,3 +29,4 @@ class ReprTestCase(TestCase):
         filtered = self.models.Tree.objects.filter(color="red")
         self.assertEqual(qs_repr(filtered),
                          """Tree.objects.filter(color='red')""")
+        self.assert_reproducible(filtered)
